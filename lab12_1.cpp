8bb459a57d7c76d90dcbc1fd9b61c5a525adfc0a
nperaya <pink.nongpink2547@gmail.com>
2023-01-06 17:09:03 +0700

    เสร็จ

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,8 +1,25 @@
-#include<isotream>
-
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
 using namespace std;
 
-
-cout << "Press Enter 3 times to reveal your future.";
-
-cout << "You will get A in this 261102.";
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    for (int y=0; y<=2; y++){
+        cin.get();
+    }
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0) cout << "You will get A in this 261102.\n";
+    else if(x == 1) cout << "You will get B+ in this 261102.\n";
+    else if(x == 2) cout << "You will get B in this 261102.\n";
+    else if(x == 3) cout << "You will get C+ in this 261102.\n";
+    else if(x == 4) cout << "You will get C in this 261102.\n";
+    else if(x == 5) cout << "You will get D+ in this 261102.\n";
+    else if(x == 6) cout << "You will get D in this 261102.\n";
+    else if(x == 7) cout << "You will get F in this 261102.\n";
+    else if(x == 8) cout << "You will get W in this 261102.\n";
+    return 0;
+}
